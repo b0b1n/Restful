b5f456a9bd7241f20a783a467172c610c8b7af7e
b0b1n <sohaibskious@gmail.com>
2022-12-15 11:55:54 +0100

    Final Version of the app

--- src/main/java/com/skious/restful/controller/GreetingController.java
+++ src/main/java/com/skious/restful/controller/GreetingController.java
@@ -2,6 +2,7 @@ package com.skious.restful.controller;
 
 import com.skious.restful.model.Greeting;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,7 +16,11 @@ public class GreetingController {
     private final AtomicLong counter = new AtomicLong();
 
     @GetMapping("/greeting")
-    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+    public Greeting getGreeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+    @PostMapping("/greeting")
+    public Greeting postGreeting(@RequestParam(value = "name", defaultValue = "World") String name) {
         return new Greeting(counter.incrementAndGet(), String.format(template, name));
     }
 
